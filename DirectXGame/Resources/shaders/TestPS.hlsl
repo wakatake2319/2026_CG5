c8012fa673c766a1645ce557696f4c4bc5abe6d8
takeuti2319 <k024g0068@g.neec.ac.jp>
2026-06-17 10:18:23 +0900

    00_08課題

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,11 +1,15 @@
+#include "Test.hlsli"
+
 struct PixelShaderOutput
 {
     float32_t4 color : SV_TARGET0;
 };
 
-PixelShaderOutput main()
+PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
-    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+    float32_t2 uv = input.texcoord;
+    
+    output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
     return output;
 }
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -14,11 +14,15 @@ using namespace KamataEngine;
 void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps) {
 	// InputLayoutの作成
 #pragma region InputLayout
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
 	inputElementDescs[0].SemanticName = "POSITION";                        // 頂点の意味を示す文字列
 	inputElementDescs[0].SemanticIndex = 0;                                // 同じ意味の頂点が複数ある場合の識別番号
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;          // データの形式
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT; // 頂点データ内のオフセット
+	inputElementDescs[1].SemanticName = "TEXCOORD";                        // 頂点の意味を示す文字列
+	inputElementDescs[1].SemanticIndex = 0;                                // 同じ意味の頂点が複数ある場合の識別番号
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;                // データの形式
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT; // 頂点データ内のオフセット
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;    // 頂点の要素の配列
 	inputLayoutDesc.NumElements = _countof(inputElementDescs); // 頂点の要素数
@@ -121,14 +125,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// Vertex4 > VertexData に変更して利用する
 	struct VertexData {
 		Vector4 position; // 頂点の位置
+		Vector2 texcoord; // 頂点のテクスチャ座標
 	};
 
 	// 頂点データの準備
 	VertexData vertices[] = {
-		{-1.0f,  1.0f,  0.0f, 1.0f}, // 左上
-	    {1.0f,  1.0f,  0.0f, 1.0f}, // 右上
-	    {-1.0f, -1.0f,  0.0f, 1.0f}, // 左下	
-		{1.0f,  -1.0f, 0.0f, 1.0f}, // 右下
+	    {{-1.0f, 1.0f, 0.0f, 1.0f}, {0.0f, 0.0f}}, // 左上
+	    {{1.0f,  1.0f,  0.0f, 1.0f}, {1.0f, 0.0f}}, // 右上
+	    {{-1.0f, -1.0f,  0.0f, 1.0f}, {0.0f, 1.0f}}, // 左下	
+		{{1.0f,  -1.0f, 0.0f, 1.0f}, {1.0f, 1.0f}}, // 右下
 	};
 #pragma endregion
 
