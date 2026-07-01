fd141281ed58b5d3217381ccf6ee85ded5f7db73
takeuti2319 <k024g0068@g.neec.ac.jp>
2026-07-01 12:59:28 +0900

    p7の途中

---
+++ DirectXGame/Resources/shaders/GaussianFilter.PS.hlsl
@@ -0,0 +1,59 @@
+#include "Test.hlsli"
+
+Texture2D<float32_t4> gTexture : register(t0); // SRV      register => t
+SamplerState gSampler : register(s0); // Sampler  register => s
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    
+    static const float32_t2 kIndex3x3[3][3] =
+    {
+        { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
+        { { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f } },
+        { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } }
+    };
+
+    static const float32_t kernel3x3[3][3] =
+    {
+        { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+        { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
+        { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f }
+    };
+    
+    
+    uint32_t width, height;
+    gTexture.GetDimensions(width, height);
+    float32_t2 uvStepSize = float32_t2(rcp(width), rcp(height));
+    
+    PixelShaderOutput output;
+    output.color.rgb = float32_t3(0.0f, 0.0f, 0.0f);
+    output.color.a = 1.0f;
+    
+    static const float32_t PI = 3.14159265358979323846f;
+    
+    float gauss(float x, float y, float sigma)
+    {
+        
+        
+    }
+    
+    
+    float32_t weight = 0.0f;
+    float32_t kernel3x3[3][3];
+    
+    for (int32_t x = 0; x < 3; ++x)
+    {
+        for (int32_t y = 0; y < 3; ++y)
+        {
+            kernel3x3[x][y] = gauss(kIndex3x3[x][y].x, kIndex3x3[x][y].y, 2.0f);
+        }
+        
+    }
+    
+    return output;
+}
